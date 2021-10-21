dac2a36182cfa640afdeb57588a0f03823ae1e95
Nurkyz-Chynybaeva <nurkyzchynybaeva29@gmail.com>
2021-10-21 18:20:14 +0600

    second method

--- 201021/app/src/main/java/com/example/a201021/MainActivity.kt
+++ 201021/app/src/main/java/com/example/a201021/MainActivity.kt
@@ -2,13 +2,47 @@ package com.example.a201021
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.appcompat.widget.AppCompatButton
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main2)
 
-    val tW = findViewById<TextView>(R.id.txt)
+        val btn1 = findViewById<AppCompatButton>(R.id.btn1)
+        val btn2 = findViewById<AppCompatButton>(R.id.btn2)
+        val btn3 = findViewById<AppCompatButton>(R.id.btn3)
 
+        btn1.setOnClickListener(this)
+        btn2.setOnClickListener(this)
+        btn3.setOnClickListener(this)
     }
+        override fun onClick(view: View) {
+
+      val msg=  when(view.id){
+            R.id.btn1 -> "btn1"
+            R.id.btn2 -> "btn2"
+            R.id.btn3 -> "btn3"
+          else -> "unknown"
+        }
+        val textV = findViewById<TextView>(R.id.text)
+        textV.text = msg
+
+        }
+
+
+
+//    fun onClickEvent(view:View){                                         старый метод
+//      val msg=  when(view.id){
+//            R.id.btn1 -> "btn1"
+//            R.id.btn2 -> "btn2"
+//            R.id.btn3 -> "btn3"
+//          else -> "unknown"
+//        }
+//        val textV = findViewById<TextView>(R.id.text)
+//        textV.text = msg
+//    }
+
 }
