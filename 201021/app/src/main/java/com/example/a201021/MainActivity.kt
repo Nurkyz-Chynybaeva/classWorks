cafb1b8e6e82a9fcf266f58aa2d2aebcbc13a6ba
Nurkyz-Chynybaeva <nurkyzchynybaeva29@gmail.com>
2021-10-21 14:13:00 +0600

    20.10.21

---
+++ 201021/app/src/main/java/com/example/a201021/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.a201021
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    val tW = findViewById<TextView>(R.id.txt)
+
+    }
+}
