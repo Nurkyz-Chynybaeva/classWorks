81f95a47dedeba7e298941ef72e3457df71a7272
Nurkyz-Chynybaeva <nurkyzchynybaeva29@gmail.com>
2021-10-22 14:04:32 +0600

    20.10.21

--- 201021/app/src/main/java/com/example/a201021/MainActivity.kt
+++ 201021/app/src/main/java/com/example/a201021/MainActivity.kt
@@ -1,36 +1,71 @@
 package com.example.a201021
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatButton
+import org.w3c.dom.Text
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+class MainActivity : AppCompatActivity(){              //View.OnClickListener
+
+private lateinit var textV:TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+
+        textV = findViewById(R.id.text)
+
+
 
         val btn1 = findViewById<AppCompatButton>(R.id.btn1)
         val btn2 = findViewById<AppCompatButton>(R.id.btn2)
         val btn3 = findViewById<AppCompatButton>(R.id.btn3)
 
-        btn1.setOnClickListener(this)
-        btn2.setOnClickListener(this)
-        btn3.setOnClickListener(this)
-    }
-        override fun onClick(view: View) {
-
-      val msg=  when(view.id){
-            R.id.btn1 -> "btn1"
-            R.id.btn2 -> "btn2"
-            R.id.btn3 -> "btn3"
-          else -> "unknown"
+        btn1.setOnClickListener{
+            textV.text = "click 1"
+            Toast.makeText(this, "click 1", Toast.LENGTH_SHORT).show()
         }
-        val textV = findViewById<TextView>(R.id.text)
-        textV.text = msg
-
+        btn2.setOnClickListener{
+            textV.text = "click 2"
+            Toast.makeText(this, "click 1", Toast.LENGTH_LONG).show()
         }
+        btn3.setOnClickListener{
+            textV.text = "click 3"
+            Toast.makeText(this, "click 1", Toast.LENGTH_SHORT).show()
+        }
+
+
+   }
+
+
+
+
+//        val btn1 = findViewById<AppCompatButton>(R.id.btn1)
+//        val btn2 = findViewById<AppCompatButton>(R.id.btn2)                     java style
+//        val btn3 = findViewById<AppCompatButton>(R.id.btn3)
+//
+//        btn1.setOnClickListener(this)
+//        btn2.setOnClickListener(this)
+//        btn3.setOnClickListener(this)
+//    }
+//
+//    override fun onClick(view: View) {
+//      val msg=  when(view.id){
+//            R.id.btn1 -> "btn1"
+//            R.id.btn2 -> "btn2"
+//            R.id.btn3 -> "btn3"
+//          else -> "unknown"
+//        }
+//
+//        textV.text = msg
+//
+//    }
+
+
 
 
 
@@ -45,4 +80,4 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 //        textV.text = msg
 //    }
 
-}
+    }
