1c98faadedf714a93e38cfb3406e09453df36590
Nurkyz-Chynybaeva <nurkyzchynybaeva29@gmail.com>
2021-11-04 20:51:07 +0600

    11th lesson

---
+++ app/src/main/java/com/example/lesson11_29_10_21/MainActivity2.kt
@@ -0,0 +1,14 @@
+package com.example.lesson11_29_10_21
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity2 : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
+
+
