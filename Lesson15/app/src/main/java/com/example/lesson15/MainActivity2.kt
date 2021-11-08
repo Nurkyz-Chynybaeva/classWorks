1e4b82c4d226f5e21b46330d5994153870402505
Nurkyz-Chynybaeva <nurkyzchynybaeva29@gmail.com>
2021-11-08 17:01:45 +0600

    15th lesson

---
+++ Lesson15/app/src/main/java/com/example/lesson15/MainActivity2.kt
@@ -0,0 +1,17 @@
+package com.example.lesson15
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity2 : AppCompatActivity(), OnButtonClicked {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+
+    override fun onButtonClicked(enteredText: String) {
+        val fragment2 = supportFragmentManager.findFragmentById(R.id.fragment_container2) as Fragment2
+        fragment2.setText(enteredText)
+    }
+}
---
+++ Lesson15/app/src/main/java/com/example/lesson15/OnButtonClicked.kt
@@ -0,0 +1,5 @@
+package com.example.lesson15
+
+interface OnButtonClicked {
+    fun onButtonClicked(enteredText: String)
+}
